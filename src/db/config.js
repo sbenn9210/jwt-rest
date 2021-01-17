0e36f84127bc22c5d5af69b318df037073b7518e
hackerrank <me@hackerrank.com>
2021-01-17 15:13:19 -0600

    update config.js

--- src/db/config.js
+++ src/db/config.js
@@ -1,27 +1,3 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
-
 require("dotenv").config();
 module.exports = {
  development: {
@@ -33,3 +9,8 @@ module.exports = {
    protocol: "postgres",
  },
 };
+
+
+
+
+
